e379772394dd651c560ce7c3cc8042a97376ca8f
Yuki Nagae <yuki.nagae1130@gmail.com>
2015-08-23 01:33:50 +1000

    add sample search tool bar

--- src/app/components/app-left-nav.jsx
+++ src/app/components/app-left-nav.jsx
@@ -1,5 +1,6 @@
 let React = require('react');
 let { MenuItem, LeftNav, Styles } = require('material-ui');
+let { Colors, Typography, Spacing } = Styles;
 
 let menuItems = [
     { text: 'Search'},
@@ -8,9 +9,31 @@ let menuItems = [
 
 class AppLeftNav extends React.Component {
 
+    getStyles() {
+        return {
+            cursor: 'pointer',
+            fontSize: '24px',
+            color: Typography.textFullWhite,
+            lineHeight: Spacing.desktopKeylineIncrement + 'px',
+            fontWeight: Typography.fontWeightLight,
+            backgroundColor: Colors.cyan500,
+            paddingLeft: Spacing.desktopGutter,
+            paddingTop: '0px',
+            marginBottom: '8px',
+        };
+    }
+
     render() {
+
+        let header = (
+            <div style={this.getStyles()} onTouchTap={this._onHeaderClick}>
+                HouzeHub
+            </div>
+        );
+
         return (
             <LeftNav
+            header={header}
             ref="leftNav"
             docked={false}
             menuItems={menuItems}
--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -2,9 +2,19 @@
 
 let React = require('react');
 let AppLeftNav = require('./app-left-nav');
-let { AppBar, AppCanvas, IconButton, Menu, Styles } = require('material-ui');
+let { AppBar, AppCanvas, IconButton, Menu, Styles, Toolbar, ToolbarGroup, DropDownMenu, TextField, RaisedButton } = require('material-ui');
 let { Colors, Typography } = Styles;
 let ThemeManager = new Styles.ThemeManager();
+
+let filterOptions = [
+    { payload: '1', text: 'All Broadcasts' },
+    { payload: '2', text: 'All Voice' },
+    { payload: '3', text: 'All Text' },
+    { payload: '4', text: 'Complete Voice' },
+    { payload: '5', text: 'Complete Text' },
+    { payload: '6', text: 'Active Voice' },
+    { payload: '7', text: 'Active Text' },
+];
 
 class Main extends React.Component {
 
@@ -19,14 +29,37 @@ class Main extends React.Component {
     }
   }
 
+  getStyles() {
+    return {
+      cursor: 'pointer',
+      color: Typography.textFullWhite,
+      fontWeight: Typography.fontWeightLight,
+      backgroundColor: Colors.cyan500,
+    };
+  }
+
   render() {
 
     return (
       <AppCanvas>
         <AppBar
-        title="HouzeHub"
         onLeftIconButtonTouchTap={this._onLeftIconButtonTouchTap}
-        />
+        >
+          <Toolbar style={this.getStyles()}>
+            <ToolbarGroup key={0} float="left">
+              <DropDownMenu menuItems={filterOptions} />
+            </ToolbarGroup>
+            <ToolbarGroup key={1} float="left">
+              <DropDownMenu menuItems={filterOptions} />
+            </ToolbarGroup>
+            <ToolbarGroup key={2} float="left">
+              <TextField hintText="Hint Text" />
+            </ToolbarGroup>
+            <ToolbarGroup key={3} float="left">
+              <RaisedButton label="Search" />
+            </ToolbarGroup>
+          </Toolbar>
+        </AppBar>
 
       <AppLeftNav ref="leftNav" />
 
