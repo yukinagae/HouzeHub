ae44a2fb12068be0f7fea0d122dcdcb08be55147
Yuki Nagae <yuki.nagae1130@gmail.com>
2015-08-22 23:12:48 +1000

    remove unused components

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -1,63 +1,32 @@
 /** In this file, we create a React component which incorporates components provided by material-ui */
 
 let React = require('react');
-let mui = require('material-ui');
-let RaisedButton = mui.RaisedButton;
-let Dialog = mui.Dialog;
-let ThemeManager = new mui.Styles.ThemeManager();
-let Colors = mui.Styles.Colors;
+let { AppBar, AppCanvas, IconButton, Menu, Styles } = require('material-ui');
+let { Colors, Typography } = Styles;
+let ThemeManager = new Styles.ThemeManager();
 
-let Main = React.createClass({
-
-  childContextTypes: {
-    muiTheme: React.PropTypes.object,
-  },
+class Main extends React.Component {
 
   getChildContext() {
     return {
       muiTheme: ThemeManager.getCurrentTheme(),
-    };
-  },
-
-  componentWillMount() {
-    ThemeManager.setPalette({
-      accent1Color: Colors.deepOrange500,
-    });
-  },
+    }
+  }
 
   render() {
 
-    let containerStyle = {
-      textAlign: 'center',
-      paddingTop: '200px',
-    };
-
-    let standardActions = [
-      { text: 'Okay' },
-    ];
-
     return (
-      <div style={containerStyle}>
-        <Dialog
-          title="Super Secret Password"
-          actions={standardActions}
-          ref="superSecretPasswordDialog">
-          1-2-3-4-5
-        </Dialog>
+      <AppCanvas>
+        <AppBar title="HouzeHub"/>
 
-        <h1>material-ui</h1>
-        <h2>example project</h2>
-
-        <RaisedButton label="Super Secret Password" primary={true} onTouchTap={this._handleTouchTap} />
-
-      </div>
+      </AppCanvas>
     );
-  },
+  }
 
-  _handleTouchTap() {
-    this.refs.superSecretPasswordDialog.show();
-  },
+}
 
-});
+Main.childContextTypes = {
+    muiTheme: React.PropTypes.object,
+};
 
 module.exports = Main;
